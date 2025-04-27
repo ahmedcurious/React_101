0cb0e5d0a1d384e99c49ff3882ebb7a23b970ba7
ahmedcurious <ahmed_naseem1@outlook.com>
2025-04-27 16:47:54 +0500

    05_paswwordGeneraotor_boilerplate

---
+++ 05_passwordGenerator/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col gap-12 text-white bg-slate-900 items-center justify-center w-full h-screen">
+      <h1 className="text-[100px] font-semibold">Hello Password Changer</h1>
+    </div>
+  );
+}
+
+export default App;
