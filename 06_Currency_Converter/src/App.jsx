544ddbea7c9c31e30ccb0e84785c794b6931bf5f
ahmedcurious <ahmed_naseem1@outlook.com>
2025-04-29 00:01:09 +0500

    06_Currency_converter_project_structure

---
+++ 06_Currency_Converter/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="w-full h-screen flex flex-col items-center justify-center
+      gap-9 bg-slate-900 text-white"
+    >
+      <h1>Hello Currency Converter</h1>
+    </div>
+  );
+}
+
+export default App;
